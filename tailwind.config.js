94a2592d2b795430f088825e8e042a2c44a0e4e2
Marcelo Sarinho <marceloactsarinho@gmail.com>
2024-03-08 15:55:33 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Lato", "sans-serif"],
+      },
+      colors: {
+        "dark-100": "#121212",
+        "dark-200": "#282828",
+        "dark-300": "#3F3F3F",
+        "aqua-green-200": "#01D2C7",
+        "aqua-green-300": "#00C5BB",
+        "purple-gray-800": "#4F4350",
+      },
+    },
+  },
+  plugins: [],
+  darkMode: "selector",
+};
